a80b15cb62d9d83e0ff13e98d14d8d2a22d6b746
Dainsleif <dainsleif@yeah.net>
2025-12-12 23:34:31 +0800

    1

--- node-functions/ping/[[server]].ts
+++ node-functions/ping/[[server]].ts
@@ -1,7 +1,9 @@
 import mcping from '../../src/mcping';
 import Logger from '../../src/libs/Logger';
 
-export function onRequestGet(ctx: any): Promise<Response> {
+export async function onRequest(ctx: any): Promise<Response> {
+    if (ctx.request.method !== 'GET') return new Response('Method Not Allowed', { status: 405 });
+
     const params = ctx.params.server;
     const server: string = Array.isArray(params) ? params[0] : params;
 
